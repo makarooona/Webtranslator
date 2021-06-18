6e905376fd724e414b65fea9dc0cb42a673ee158
mkarooona <ibrahimtanicho@gmail.com>
2021-06-19 01:41:31 +0200

    config update

--- api/config.php
+++ api/config.php
@@ -3,8 +3,8 @@
 
 class Config{
     const DB_HOST = "localhost";
-    CONST DB_USERNAME = "root";
-    CONST DB_PASSWORD = "Bosanski123";
+    CONST DB_USERNAME = "webtranslator";
+    CONST DB_PASSWORD = "webtranslator";
     CONST DB_SCHEME = "webtranslator";
 }
 
