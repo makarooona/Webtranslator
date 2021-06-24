8d71f39a33c6c238d681a290b7b457c28ef9ff6d
mkarooona <ibrahimtanicho@gmail.com>
2021-06-24 12:31:54 +0200

    setting up flight php

--- api/index.php
+++ api/index.php
@@ -3,10 +3,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once dirname(__File__)."/dao/BaseDao.class.php";
+require dirname(__FILE__).'/../vendor/autoload.php';
 
-$teezhasan = new BaseDao();
+Flight::route('/', function(){
+    echo 'hello world!';
+});
 
-echo "HELLO FROM API";
+Flight::route('/hello2', function(){
+    echo 'hello world2!';
+});
 
+
+Flight::start();
 ?>
