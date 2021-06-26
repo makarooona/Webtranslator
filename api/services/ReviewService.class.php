08963217705d83d047708921968e214a5735d494
mkarooona <ibrahimtanicho@gmail.com>
2021-06-26 16:48:47 +0200

    adding Services layer

---
+++ api/services/ReviewService.class.php
@@ -0,0 +1,31 @@
+<?php
+require_once dirname(__FILE__)."/BaseService.class.php";
+require_once dirname(__FILE__)."/../dao/Account.class.php";   
+   
+class ReviewService extends BaseService{
+    public function __construct(){
+    $this->dao = new ReviewDao();
+    }
+
+    public function get_reviews($search, $offset, $limit, $order){
+        if ($search){
+          return $this->dao->get_reviews($search, $offset, $limit, $order);
+        }else{
+          return $this->dao->get_all($offset, $limit, $order);
+        }
+      }
+
+    public function add($review){
+        // validation of review data
+        if (!isset($review['name'])) throw new Exception("Name is missing");
+        $review['created_at'] = date(Config::DATE_FORMAT);
+        return parent::add($review);
+      } 
+
+
+
+
+    }
+
+
+?>
