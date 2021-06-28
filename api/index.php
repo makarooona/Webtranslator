de57d38a6dfd62dcc4c459d12761feb6abbcf40e
mkarooona <ibrahimtanicho@gmail.com>
2021-06-28 03:56:54 +0200

    setting up jwt and middleware

--- api/index.php
+++ api/index.php
@@ -8,12 +8,32 @@ require_once dirname(__FILE__).'/services/AccountService.class.php';
 require_once dirname(__FILE__).'/services/UserService.class.php';
 require_once dirname(__FILE__).'/services/ReviewService.class.php';
 
+Flight::set('flight.log_errors', TRUE);
+
+/* error handling for our API */
+Flight::map('error', function(Exception $ex){
+  Flight::json(["message" => $ex->getMessage()], $ex->getCode() ? $ex->getCode() : 500);
+});
+
+
 /* utility function for reading query parameters from URL */
 Flight::map('query', function($name, $default_value = NULL){
     $request = Flight::request();
     $query_param = @$request->query->getData()[$name];
     $query_param = $query_param ? $query_param : $default_value;
     return urldecode($query_param);
+  });
+
+ /* utility function for getting header parameters */
+Flight::map('header', function($name){
+    $headers = getallheaders();
+    return @$headers[$name];
+  });
+
+/* utility function for generating JWT token */
+Flight::map('jwt', function($user){
+    $jwt = \Firebase\JWT\JWT::encode(["exp" => (time() + Config::JWT_TOKEN_TIME), "id" => $user["id"], "aid" => $user["account_id"], "r" => $user["role"]], Config::JWT_SECRET);
+    return ["token" => $jwt];
   });
 
 Flight::route('GET /swagger', function(){
